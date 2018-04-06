9c936515b2d85580b55549a7005c20d09db4dbf6
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-04-05 20:48:30 -0700

    spotify is integrated, movies are integrated,  still need to fix when no input is received, to do integrate twitter

--- liri.js
+++ liri.js
@@ -2,14 +2,49 @@ require("dotenv").config();
 //var spotify = new Spotify(keys.spotify);
 //var client = new Twitter(keys.twitter);
 
+var fs = require("fs");
 var command = process.argv[2];
+
 
 if (command === "my-tweets") {
     console.log("tweet tweet");
 }
 if (command === "spotify-this-song") {
-    console.log("do rey mi");
+    //console.log("do rey mi");
+    var songToSearch = "";
+    for (var i = 3; i < process.argv.length; i++) {
+        songToSearch = songToSearch + process.argv[i] + " ";
+    }
+    if (songToSearch === "") {
+        songToSearch = "The Sign";
+    }
+    var keys = require("./keys.js");
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track', query: songToSearch }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        //  artist(s)
+        //var artistsArr = [];
+        for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
+            artist = data.tracks.items[0].artists[i].name
+            console.log("Song by: " + artist);
+        }
+        //  The song's name
+        console.log("Song Title: " + data.tracks.items[0].name);
+        //  A preview link of the song from Spotify
+        console.log("Preview Here " + data.tracks.items[0].preview_url);
+
+        //  The album that the song is from
+        console.log("Album: " + data.tracks.items[0].album.name)
+
+
+
+    });
 }
+
 if (command === "movie-this") {
     var movieName = "";
     for (var i = 3; i < process.argv.length; i++) {
@@ -44,5 +79,44 @@ if (command === "movie-this") {
     })
 }
 if (command === "do-what-it-says") {
-    console.log("simon says");
+    //console.log("simon says");
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        //console.log(data);
+
+        var dataArr = data.split(",");
+
+        //console.log(dataArr);
+        //console.log(dataArr[1]);
+        var spotifyThis = dataArr[1];
+        var keys = require("./keys.js");
+        var Spotify = require('node-spotify-api');
+        var spotify = new Spotify(keys.spotify);
+
+        spotify.search({ type: 'track', query: spotifyThis }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            //  artist(s)
+            //var artistsArr = [];
+            for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
+                artist = data.tracks.items[0].artists[i].name
+                console.log("Song by: " + artist);
+            }
+            //  The song's name
+            console.log("Song Title: " + data.tracks.items[0].name);
+            //  A preview link of the song from Spotify
+            console.log("Preview Here " + data.tracks.items[0].preview_url);
+
+            //  The album that the song is from
+            console.log("Album: " + data.tracks.items[0].album.name)
+
+
+
+        });
+    })
 }
