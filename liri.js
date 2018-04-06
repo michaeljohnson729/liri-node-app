12f18416f3c1c01e3cdbe9b7bbde1e800c3bbf62
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-04-06 14:47:28 -0700

    DONE

--- liri.js
+++ liri.js
@@ -1,87 +1,116 @@
 require("dotenv").config();
-//var spotify = new Spotify(keys.spotify);
-//var client = new Twitter(keys.twitter);
-
+var inquirer = require("inquirer");
 var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var Twitter = require("twitter");
+
 var command = process.argv[2];
 
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
 if (command === "my-tweets") {
-    console.log("tweet tweet");
-}
-if (command === "spotify-this-song") {
-    //console.log("do rey mi");
-    var songToSearch = "";
-    for (var i = 3; i < process.argv.length; i++) {
-        songToSearch = songToSearch + process.argv[i] + " ";
-    }
-    if (songToSearch === "") {
-        songToSearch = "The Sign";
-    }
-    var keys = require("./keys.js");
-    var Spotify = require('node-spotify-api');
-    var spotify = new Spotify(keys.spotify);
-
-    spotify.search({ type: 'track', query: songToSearch }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
+    //console.log("tweet tweet");
+    var params = { screen_name: 'michaelsadummy' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var k = 0; k < tweets.length; k++) {
+                console.log("Tweet: "+tweets[k].text);
+                console.log("Created at: "+tweets[k].created_at);
+            }
         }
-        //  artist(s)
-        
-        for (var j = 0; j < data.tracks.items.length; j++) {
-            var artistsArr = [];
-            for (var i = 0; i < data.tracks.items[j].artists.length; i++) {
-            artist = data.tracks.items[j].artists[i].name
-            artistsArr.push(artist);
-        }
-        console.log("-------------------------------");
-        console.log("Artist(s): "+artistsArr.join(", "));
-        //  The song's name
-        console.log("Song Title: " + data.tracks.items[j].name);
-        //  A preview link of the song from Spotify
-        console.log("Preview Here " + data.tracks.items[j].preview_url);
-
-        //  The album that the song is from
-        console.log("Album: " + data.tracks.items[j].album.name)
-        console.log("-------------------------------");
-    }
-
-
     });
 }
-
-if (command === "movie-this") {
-    var movieName = "";
-    for (var i = 3; i < process.argv.length; i++) {
-        movieName = movieName + process.argv[i] + " ";
-    }
-    if (movieName === "") {
-        movieName = "Mr. Nobody";
-    };
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    //console.log(queryUrl);
-    var request = require("request");
-    request(queryUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            //             Title of the movie.
-            console.log("Title: " + JSON.parse(body).Title);
-            //   * Year the movie came out.
-            console.log(JSON.parse(body).Title + " was released in " + JSON.parse(body).Year + ".");
-            //   * IMDB Rating of the movie.
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating)
-            //   * Rotten Tomatoes Rating of the movie.
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-            //   * Country where the movie was produced.
-            console.log("This movie was produced in " + JSON.parse(body).Country);
-            //   * Language of the movie.
-            console.log("This movie is in " + JSON.parse(body).Language);
-            //   * Plot of the movie.
-            console.log("Plot: " + JSON.parse(body).Plot);
-            //   * Actors in the movie.
-            console.log("Actors in this movie: " + JSON.parse(body).Actors);
-
+if (command === "spotify-this-song") {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What song would you like search for?",
+            name: "song"
         }
+    ]).then(function (inquirerResponse) {
+        var songToSearch = "";
+        if (inquirerResponse.song === "") {
+            songToSearch = "The Sign";
+        }
+        else {
+            songToSearch = inquirerResponse.song;
+        }
+
+        
+
+        spotify.search({ type: 'track', query: songToSearch }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            //  artist(s)
+
+            for (var j = 0; j < 5; j++) {
+                var artistsArr = [];
+                for (var i = 0; i < data.tracks.items[j].artists.length; i++) {
+                    artist = data.tracks.items[j].artists[i].name
+                    artistsArr.push(artist);
+                }
+                console.log("-------------------------------");
+                console.log("Artist(s): " + artistsArr.join(", "));
+                //  The song's name
+                console.log("Song Title: " + data.tracks.items[j].name);
+                //  A preview link of the song from Spotify
+                console.log("Preview Here " + data.tracks.items[j].preview_url);
+
+                //  The album that the song is from
+                console.log("Album: " + data.tracks.items[j].album.name)
+                console.log("-------------------------------");
+            }
+
+
+        });
     })
+
+}
+if (command === "movie-this") {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What movie would you like to search for?",
+            name: "movie"
+        }
+    ]).then(function (inquirerResponse) {
+        var movieName = "";
+        movieName = inquirerResponse.movie;
+
+        if (movieName === "") {
+            movieName = "Mr. Nobody";
+        }
+
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        //console.log(queryUrl);
+        var request = require("request");
+        request(queryUrl, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                //             Title of the movie.
+                console.log("Title: " + JSON.parse(body).Title);
+                //   * Year the movie came out.
+                console.log(JSON.parse(body).Title + " was released in " + JSON.parse(body).Year + ".");
+                //   * IMDB Rating of the movie.
+                console.log("IMDB Rating: " + JSON.parse(body).imdbRating)
+                //   * Rotten Tomatoes Rating of the movie.
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                //   * Country where the movie was produced.
+                console.log("This movie was produced in " + JSON.parse(body).Country);
+                //   * Language of the movie.
+                console.log("This movie is in " + JSON.parse(body).Language);
+                //   * Plot of the movie.
+                console.log("Plot: " + JSON.parse(body).Plot);
+                //   * Actors in the movie.
+                console.log("Actors in this movie: " + JSON.parse(body).Actors);
+
+            }
+        })
+    })
+
 }
 if (command === "do-what-it-says") {
     //console.log("simon says");
@@ -98,9 +127,7 @@ if (command === "do-what-it-says") {
         //console.log(dataArr);
         //console.log(dataArr[1]);
         var spotifyThis = dataArr[1];
-        var keys = require("./keys.js");
-        var Spotify = require('node-spotify-api');
-        var spotify = new Spotify(keys.spotify);
+        
 
         spotify.search({ type: 'track', query: spotifyThis }, function (err, data) {
             if (err) {
@@ -116,11 +143,8 @@ if (command === "do-what-it-says") {
             console.log("Song Title: " + data.tracks.items[0].name);
             //  A preview link of the song from Spotify
             console.log("Preview Here " + data.tracks.items[0].preview_url);
-
             //  The album that the song is from
             console.log("Album: " + data.tracks.items[0].album.name)
-
-
 
         });
     })
